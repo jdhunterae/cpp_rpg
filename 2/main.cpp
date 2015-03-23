4a9ce673e17aca15a5bbb91dd9c7aadfba874866
Andrew Pomerleau <arpomerleau@gmail.com>
2015-03-23 10:10:44 -0400

    Roll back 2/main.cpp

--- 2/main.cpp
+++ 2/main.cpp
@@ -10,45 +10,6 @@
 #include "creature.hpp"
 
 
-Creature dialogue_newchar();
-
 int main() {
-  Creature player;
-  srand(time(NULL));
-
-  int result = Dialogue(
-    "Welcome!",
-    {"New Game"}).activate();
-
-  switch(result) {
-    case 1: player = dialogue_newchar(); break;
-    default: return 0; break;
-  }
-
-  std::cout << player.name << ": " << player.className << std::endl;
-
   return 0;
-}
-
-Creature dialogue_newchar() {
-  std::cout << "Choose your name" << std::endl;
-  std::string name;
-  std::cin >> name;
-
-  int result = Dialogue(
-    "Choose your class",
-    {"Fighter", "Rogue"}).activate();
-
-  switch(result)
-  {
-    case 1:
-      return Creature(name, 35, 20, 10, 5, 10.0, 1, "Fighter");
-      break;
-   case 2:
-      return Creature(name, 30, 5, 10, 20, 15.0, 1, "Fighter");
-      break;
-    default:
-      return Creature(name, 30, 10, 10, 10, 10.0, 1, "Adventurer");
-    break;
-  }
 }
